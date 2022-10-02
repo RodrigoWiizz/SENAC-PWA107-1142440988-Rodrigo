996438a3b390d1ab154581037fe0d6c610b0b803
Rodrigo Lopes <rodrigo.lszsouza@gmail.com>
2022-10-01 22:02:06 -0300

    ado2

--- src/paginas/Movimentacao/index.js
+++ src/paginas/Movimentacao/index.js
@@ -1,6 +1,16 @@
+import { Container} from "react-bootstrap";
+
 function Movimentacao() {
     return (
-      <p>Bem vindo a Movimentação</p>
+      <Container className="main-container d-grid h-100">
+        <div className='text-center'>
+        <img 
+          className='mb-4 bootstrap-logo'
+          src="https://upload.wikimedia.org/wikipedia/commons/7/71/GNUstep_logo.svg"
+          alt="Bootstrap 5"/>
+          <h3>Conta corrente</h3>
+        </div>
+      </Container>
     );
   }
   
